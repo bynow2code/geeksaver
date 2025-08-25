4ab44caf298e07141a2a2f7cd748bd14dc5776c1
bynow2code <36981638+bynow2code@users.noreply.github.com>
2025-08-25 08:37:22 +0800

    feat:删除ua

--- internal/geekbang/client.go
+++ internal/geekbang/client.go
@@ -9,9 +9,9 @@ var httpClient *http.Client
 
 func init() {
 	transport := &http.Transport{
-		MaxIdleConns:          3,                // 空闲时保留连接数
+		MaxIdleConns:          2,                // 空闲时保留连接数
 		IdleConnTimeout:       30 * time.Second, // 空闲连接超时时间
-		MaxConnsPerHost:       6,                // 对单个主机最多x个并发连接
+		MaxConnsPerHost:       2,                // 对单个主机最多x个并发连接
 		ResponseHeaderTimeout: 5 * time.Second,  // 响应头超时
 	}
 
