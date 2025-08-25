a6907baca401f019bba8c219b52c34c272637be3
bynow2code <36981638+bynow2code@users.noreply.github.com>
2025-08-25 09:58:58 +0800

    feat:transport

--- internal/geekbang/client.go
+++ internal/geekbang/client.go
@@ -9,10 +9,8 @@ var httpClient *http.Client
 
 func init() {
 	transport := &http.Transport{
-		MaxIdleConns:          2,                // 空闲时保留连接数
-		IdleConnTimeout:       30 * time.Second, // 空闲连接超时时间
-		MaxConnsPerHost:       2,                // 对单个主机最多x个并发连接
-		ResponseHeaderTimeout: 5 * time.Second,  // 响应头超时
+		MaxConnsPerHost:       4,               // 对单个主机最多x个并发连接
+		ResponseHeaderTimeout: 5 * time.Second, // 响应头超时
 	}
 
 	httpClient = &http.Client{
