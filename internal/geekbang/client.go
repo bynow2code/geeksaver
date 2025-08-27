528aedca522f1e509d5a7df4e4604b9f51379599
bynow2code <36981638+bynow2code@users.noreply.github.com>
2025-08-27 20:01:36 +0800

    refactor: 抽象req参数

--- internal/geekbang/client.go
+++ internal/geekbang/client.go
@@ -17,7 +17,6 @@ func (c *Client) Do(req *http.Request) (*http.Response, error) {
 }
 
 func buildCommonReqParams(req *http.Request) {
-	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Origin", "https://time.geekbang.org")
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0")
 
