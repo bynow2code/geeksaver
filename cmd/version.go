80dd786ad231c0090ea2e855ad21d970175f7570
bynow2code <36981638+bynow2code@users.noreply.github.com>
2025-09-11 03:25:41 +0800

    chore: 版本注入

--- cmd/version.go
+++ cmd/version.go
@@ -6,14 +6,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version = "v0.0.1-alpha"
+var Version = "v0.0.1-alpha"
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "当前版本",
 	Long:  `当前版本`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("当前版本：%s\n", version)
+		fmt.Printf("当前版本：%s\n", Version)
 	},
 }
 
