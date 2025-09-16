30fb93c57471f0374e9196a5e09e2e525c1c6822
bynow2code <36981638+bynow2code@users.noreply.github.com>
2025-09-17 01:44:57 +0800

    feat: 增加程序自升级

--- cmd/upgrade.go
+++ cmd/upgrade.go
@@ -1,12 +1,15 @@
 package cmd
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"runtime"
 	"strings"
 	"time"
@@ -23,34 +26,62 @@ var upgradeCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		err := doUpgrade()
 		if err != nil {
-			fmt.Println(err)
-			os.Exit(0)
+			log.Fatalln(err)
 		}
 	},
 }
 
 func init() {
-	//rootCmd.AddCommand(upgradeCmd)
+	rootCmd.AddCommand(upgradeCmd)
 }
 
 type upgradeProcessor struct {
 	releaseLatest ReleaseLatest
+	assert        Asset
 }
 
 type ReleaseLatest struct {
 	TagName     string    `json:"tag_name"`
 	PublishedAt time.Time `json:"published_at"`
-	Assets      []struct {
-		Name               string `json:"name"`
-		BrowserDownloadUrl string `json:"browser_download_url"`
-	} `json:"assets"`
+	Assets      []Asset   `json:"assets"`
+}
+
+type Asset struct {
+	Name               string `json:"name"`
+	BrowserDownloadUrl string `json:"browser_download_url"`
 }
 
 // 程序升级入口
 func doUpgrade() error {
+	processor := &upgradeProcessor{}
+
 	fmt.Printf("当前版本: %s\n", version)
 
-	progressBar := progressbar.NewOptions(1,
+	// 检查更新
+	if err := processor.checkForUpdate(); err != nil {
+		return err
+	}
+
+	fmt.Printf("最新版本: %s\n", processor.releaseLatest.TagName)
+
+	// 检查是否需要升级
+	if err := processor.checkNeedUpgrade(); err != nil {
+		return err
+	}
+
+	// 升级
+	if err := processor.upgrade(); err != nil {
+		return err
+	}
+
+	fmt.Println("升级完成")
+
+	return nil
+}
+
+// 检查更新并显示进度条
+func (u *upgradeProcessor) checkForUpdate() error {
+	checkBar := progressbar.NewOptions(1,
 		progressbar.OptionSetDescription("检查更新中..."),
 		progressbar.OptionShowBytes(false),
 		progressbar.OptionFullWidth(),
@@ -60,29 +91,13 @@ func doUpgrade() error {
 		}),
 	)
 
-	processor := &upgradeProcessor{}
-	err := processor.getReleaseLatest()
-	if err != nil {
-		return fmt.Errorf("获取新版本信息错误：%s", err)
+	// 获取最新版本信息
+	if err := u.getReleaseLatest(); err != nil {
+		return errors.New(fmt.Sprintf("获取新版本信息错误：%s", err))
 	}
-	err = progressBar.Finish()
-	if err != nil {
+
+	if err := checkBar.Finish(); err != nil {
 		return err
-	}
-
-	fmt.Printf("最新版本: %s\n", processor.releaseLatest.TagName)
-
-	upgrade, err := processor.needUpgrade()
-	if err != nil {
-		return err
-	}
-	if !upgrade {
-		fmt.Println("当前版本已经是最新版本")
-		return nil
-	}
-
-	if version == devVersion {
-		return errors.New("请使用 go install github.com/bynow2code/geeksaver@latest 的形式安装最新版")
 	}
 
 	return nil
@@ -102,14 +117,31 @@ func (u *upgradeProcessor) getReleaseLatest() error {
 		return errors.New(fmt.Sprintf("检查新版本错误: %s", resp.Status))
 	}
 
-	result, err := io.ReadAll(resp.Body)
+	newBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return err
 	}
 
-	err = json.Unmarshal(result, &u.releaseLatest)
+	if err := json.Unmarshal(newBytes, &u.releaseLatest); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// 检查是否需要升级
+func (u *upgradeProcessor) checkNeedUpgrade() error {
+	need, err := u.needUpgrade()
 	if err != nil {
 		return err
+	}
+	if !need {
+		return errors.New("当前版本已经是最新版本")
+	}
+
+	// go install 安装或 dev 不允许命令行更新
+	if version == devVersion {
+		return errors.New("请使用 go install github.com/bynow2code/geeksaver@latest 的形式安装最新版")
 	}
 
 	return nil
@@ -136,16 +168,127 @@ func (u *upgradeProcessor) needUpgrade() (bool, error) {
 	return false, nil
 }
 
+// 匹配升级文件下载地址
+func (u *upgradeProcessor) matchUpgradeUrl() error {
+	upgradeFile, err := u.getPlatformBinaryName()
+	if err != nil {
+		return err
+	}
+
+	for _, asset := range u.releaseLatest.Assets {
+		if upgradeFile == asset.Name {
+			u.assert = asset
+			return nil
+		}
+	}
+
+	return errors.New("没有匹配的升级文件")
+}
+
 // 获取平台对应的二进制文件名
-func getPlatformBinaryName(version string) (string, error) {
+func (u *upgradeProcessor) getPlatformBinaryName() (string, error) {
 	switch runtime.GOOS {
 	case "darwin":
-		return fmt.Sprintf("geeksaver-%s-macos-%s", version, runtime.GOARCH), nil
+		return fmt.Sprintf("geeksaver-%s-macos-%s", u.releaseLatest.TagName, runtime.GOARCH), nil
 	case "linux":
-		return fmt.Sprintf("geeksaver-%s-linux-%s", version, runtime.GOARCH), nil
+		return fmt.Sprintf("geeksaver-%s-linux-%s", u.releaseLatest.TagName, runtime.GOARCH), nil
 	case "windows":
-		return fmt.Sprintf("geeksaver-%s-windows-%s.exe", version, runtime.GOARCH), nil
+		return fmt.Sprintf("geeksaver-%s-windows-%s.exe", u.releaseLatest.TagName, runtime.GOARCH), nil
 	default:
 		return "", errors.New("不支持的操作系统")
 	}
+}
+
+// 升级
+func (u *upgradeProcessor) upgrade() error {
+	upgradeBar := progressbar.NewOptions(1,
+		progressbar.OptionSetDescription("升级中..."),
+		progressbar.OptionShowBytes(false),
+		progressbar.OptionFullWidth(),
+		progressbar.OptionSetTheme(progressbar.Theme{}),
+		progressbar.OptionOnCompletion(func() {
+			fmt.Fprint(os.Stderr, "\n")
+		}),
+	)
+
+	// 匹配升级文件下载地址
+	if err := u.matchUpgradeUrl(); err != nil {
+		return err
+	}
+
+	// 升级文件
+	if err := u.upgradeFile(); err != nil {
+		return err
+	}
+
+	if err := upgradeBar.Finish(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// 升级文件
+func (u *upgradeProcessor) upgradeFile() error {
+	// 下载升级文件
+	client := http.Client{Timeout: 10 * time.Second}
+	resp, err := client.Get(u.assert.BrowserDownloadUrl)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	newBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	exePath, err := os.Executable()
+	if err != nil {
+		return err
+	}
+	targetPath, err := filepath.EvalSymlinks(exePath)
+	if err != nil {
+		return err
+	}
+
+	updateDir := filepath.Dir(targetPath)
+	filename := filepath.Base(targetPath)
+
+	// 创建新升级文件
+	newPath := filepath.Join(updateDir, fmt.Sprintf(".%s.new", filename))
+	newFile, err := os.OpenFile(newPath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0755)
+	if err != nil {
+		return err
+	}
+	defer newFile.Close()
+
+	if _, err := io.Copy(newFile, bytes.NewReader(newBytes)); err != nil {
+		return err
+	}
+
+	// windows 打开文件后，需要先关闭，然后才能才能移动这个文件
+	newFile.Close()
+
+	// 删除旧文件
+	// Windows 如果重命名操作的目标文件已存在，重命名操作将会失败
+	oldPath := filepath.Join(updateDir, fmt.Sprintf(".%s.old", filename))
+	_ = os.Remove(oldPath)
+	// 备份旧文件
+	if err := os.Rename(targetPath, oldPath); err != nil {
+		return err
+	}
+
+	// 替换新文件
+	if err := os.Rename(newPath, targetPath); err != nil {
+		// 升级没有成功，恢复旧文件
+		_ = os.Rename(oldPath, targetPath)
+		return fmt.Errorf("升级失败（已回滚）：%w", err)
+	}
+
+	// 删除旧文件
+	// Windows 更新后进程仍在运行导致无法删除旧文件
+	_ = os.Remove(oldPath)
+
+	return nil
 }
